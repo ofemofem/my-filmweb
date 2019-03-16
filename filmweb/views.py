659a5ede8001c9288e2917662e8116af8bc344e7
kamil.foltyniak <folt3k@gmail.com>
2019-03-16 18:38:59 +0100

    init home page view

--- filmweb/views.py
+++ filmweb/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+movies = [
+    {
+        "title": "Stranger Things",
+        "description" : "Podczas powrotu do domu znika dwunastoletni Will Byers. Zaginięcie chłopca jest początkiem dziwnych i niebezpiecznych wydarzeń trapiących prowincjonalne miasteczko.",
+        "image_thumbnail": "https://www.kawerna.pl/wp-content/uploads/2018/02/Stranger_Things_top3.jpg",
+        "release_date": "22 marca 2015",
+    },
+        {
+        "title": "Stranger Things",
+        "description" : "Podczas powrotu do domu znika dwunastoletni Will Byers. Zaginięcie chłopca jest początkiem dziwnych i niebezpiecznych wydarzeń trapiących prowincjonalne miasteczko.",
+        "image_thumbnail": "https://www.kawerna.pl/wp-content/uploads/2018/02/Stranger_Things_top3.jpg",
+        "release_date": "22 marca 2015",
+    },
+        {
+        "title": "Stranger Things",
+        "description" : "Podczas powrotu do domu znika dwunastoletni Will Byers. Zaginięcie chłopca jest początkiem dziwnych i niebezpiecznych wydarzeń trapiących prowincjonalne miasteczko.",
+        "image_thumbnail": "https://www.kawerna.pl/wp-content/uploads/2018/02/Stranger_Things_top3.jpg",
+        "release_date": "22 marca 2015",
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>FILMWEB!</h1>')
+    context = {
+        "movies": movies
+    }
+    return render(request, 'filmweb/home/home.html', context)
