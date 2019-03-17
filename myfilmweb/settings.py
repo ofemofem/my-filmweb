de41423cfeb2cc5e678e819eaa74cf3dfc6aa000
kamil.foltyniak <folt3k@gmail.com>
2019-03-17 20:18:04 +0100

    register form view

--- myfilmweb/settings.py
+++ myfilmweb/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'filmweb.apps.FilmwebConfig',
+    'widget_tweaks',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- users/views.py
+++ users/views.py
@@ -12,6 +12,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in.{ username }')
             return redirect('login')
+            
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
