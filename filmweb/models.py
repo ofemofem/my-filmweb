f467d19f29a02ea5399b58d7f9eeb0a69931db8f
Your Name <you@example.com>
2019-03-17 14:38:01 +0100

    init register views

--- filmweb/models.py
+++ filmweb/models.py
@@ -6,6 +6,6 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image_thumbnail = models.TextField()
-    release_date = models.DateTimeField()
+    release_date = models.DateField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- filmweb/views.py
+++ filmweb/views.py
@@ -1,25 +1,6 @@
 from django.shortcuts import render
+from .models import Movie
 
-movies = [
-    {
-        "title": "Stranger Things",
-        "description" : "Podczas powrotu do domu znika dwunastoletni Will Byers. Zaginięcie chłopca jest początkiem dziwnych i niebezpiecznych wydarzeń trapiących prowincjonalne miasteczko.",
-        "image_thumbnail": "https://www.kawerna.pl/wp-content/uploads/2018/02/Stranger_Things_top3.jpg",
-        "release_date": "22 marca 2015",
-    },
-        {
-        "title": "Stranger Things",
-        "description" : "Podczas powrotu do domu znika dwunastoletni Will Byers. Zaginięcie chłopca jest początkiem dziwnych i niebezpiecznych wydarzeń trapiących prowincjonalne miasteczko.",
-        "image_thumbnail": "https://www.kawerna.pl/wp-content/uploads/2018/02/Stranger_Things_top3.jpg",
-        "release_date": "22 marca 2015",
-    },
-        {
-        "title": "Stranger Things",
-        "description" : "Podczas powrotu do domu znika dwunastoletni Will Byers. Zaginięcie chłopca jest początkiem dziwnych i niebezpiecznych wydarzeń trapiących prowincjonalne miasteczko.",
-        "image_thumbnail": "https://www.kawerna.pl/wp-content/uploads/2018/02/Stranger_Things_top3.jpg",
-        "release_date": "22 marca 2015",
-    }
-]
 
 def home(request):
-    return render(request, 'filmweb/home/home.html', {"movies": movies})
+    return render(request, 'filmweb/home/home.html', {'movies': Movie.objects.all()})
